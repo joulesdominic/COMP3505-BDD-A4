6a0297e753f97af34797a3f1441a9819e4456149
sagui535 <113629950+sagui535@users.noreply.github.com>
2025-03-15 23:15:56 -0600

    Update ChangePWSteps.java

--- src/test/java/my/domain/name/jpetstore/steps/ChangePWSteps.java
+++ src/test/java/my/domain/name/jpetstore/steps/ChangePWSteps.java
@@ -35,30 +35,27 @@ public class ChangePWSteps {
 	
 	@When("I navigate to the change password page")
 	    public void i_navigate_to_the_change_password_page() throws Throwable {
-			//driver.get("http://35.224.152.84:8080/jpetstore/actions/Account.action?editAccountForm=");
 	        driver.findElement(By.linkText("My Account")).click();
-	        //driver.findElement(By.linkText("Change Password")).click();
 	    }
 	
-	@When("I enter {string} in the new password field")
-    public void i_enter_new_password(String newpassword) throws Throwable {
-        WebElement newPasswordField = driver.findElement(By.name("password"));
-        newPasswordField.clear();
-        newPasswordField.sendKeys(newpassword);
-    }
-	
-	 @When("I enter {string} in the repeat password field")
-	    public void i_enter_repeat_password(String newpassword) throws Throwable {
-	        WebElement confirmPasswordField = driver.findElement(By.name("repeatedPassword"));
-	        confirmPasswordField.clear();
-	        confirmPasswordField.sendKeys(newpassword);
-	    }
-	 
-	 @When("I submit the password change form")
-	    public void i_submit_the_password_change_form() throws Throwable {
-	        driver.findElement(By.cssSelector("input[type='submit'][value='Save Account Information']")).click();
-	    }
-	 
+	@When("I enter {string} in the password field")
+	public void i_enter_password(String newpassword) throws Throwable {
+	    WebElement passwordField = driver.findElement(By.name("password")); // Use name instead of ID
+	    passwordField.clear();
+	    passwordField.sendKeys(newpassword);
+	}
+
+	@When("I confirm {string} in the repeat password field")
+	public void i_confirm_repeat_password(String newpassword) throws Throwable {
+	    WebElement confirmPasswordField = driver.findElement(By.name("repeatedPassword"));
+	    confirmPasswordField.clear();
+	    confirmPasswordField.sendKeys(newpassword);
+	}
+
+	@When("I submit the password change form")
+	public void i_submit_the_password_change_form() throws Throwable {
+	    driver.findElement(By.name("editAccount")).click();
+	}
 	
 	@Then("I should be directed back to main page")
 	public void i_should_be_directed_to_main_page() throws Throwable{
@@ -66,26 +63,44 @@ public class ChangePWSteps {
 			String currentUrl = driver.getCurrentUrl();
 		    assertTrue(currentUrl.equals("http://35.224.152.84:8080/jpetstore/actions/Catalog.action"));
 		} catch (Exception e) {
-			fail();
-		} finally {
-			driver.quit();
+			fail("Not redirected to main page");
 		}
 
 	}
-	@Then("I should see an error message")
-	public void i_should_see_an_error_message() throws Throwable{
-		try {
-            WebElement errorMessage = driver.findElement(By.cssSelector("div#Content li"));
-            String actualErrorMessage = errorMessage.getText();
-            String expectedErrorMessage = "Invalid username or password. Signon failed.";
+	
+	@When("I logout")
+	public void i_logout() throws Throwable {
+	    try {
+	        WebElement signOutLink = driver.findElement(By.cssSelector("div#MenuContent a[href*='signoff=']"));
+	        signOutLink.click();
+	        Thread.sleep(1000); 
+	    } catch (Exception e) {
+	        fail("Logout failed.");
+	    }
+	}
 
-            assertTrue("Error message is not displayed or incorrect", 
-                       errorMessage.isDisplayed() && actualErrorMessage.equals(expectedErrorMessage));
-        } catch (Exception e) {
-            fail("Error message not displayed or incorrect: " + e.getMessage());
-        } finally {
-            driver.quit();
-        }
+	@Then("I should see the Sign In button again")
+	public void i_should_see_the_sign_in_button_again() throws Throwable {
+	    try {
+	        WebElement signInButton = driver.findElement(By.cssSelector("div#MenuContent a[href*='signonForm=']"));
+	        assertTrue(signInButton.isDisplayed());
+	    } catch (Exception e) {
+	        fail("Sign In button not found after logout.");
+	    }
+	}
+
+	@Then("I should be able to login with {string} and {string}")
+	public void i_should_be_able_to_login_with_and(String username, String newpassword) throws Throwable {
+	    driver.get("http://35.224.152.84:8080/jpetstore/actions/Account.action?signonForm=");
+	    enterCredentials(username, newpassword);
+	    try {
+	        String currentUrl = driver.getCurrentUrl();
+	        assertTrue(currentUrl.equals("http://35.224.152.84:8080/jpetstore/actions/Catalog.action"));
+	    } catch (Exception e) {
+	        fail("Login with new password failed.");
+	    } finally {
+	        driver.quit();
+	    }
 	}
 	
 	private void enterCredentials(String username, String password) throws Throwable{
